1d3e476315b4262d869c0195b48eb0fbf2f84f16
dacc77 <123707776+dacc77@users.noreply.github.com>
2023-07-30 14:31:21 -0700

    made changes to CSS

---
+++ app.js
@@ -0,0 +1,8 @@
+//create a trivia like game.
+// based on the results of 7 different questions, you will either 
+// get 1 of 4 different groups of nature related zones with a list of activities that best match your
+//preferences 
+// question 1. do you prefer to relax in warm, hot or cold environments?
+//question 2. what is your activity level?
+//question 3. would you rather activities where you can bring your dogs?
+// question 4. are you more into group or solo activities?
